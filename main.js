dd3173218dc945960191f1a1c0b684aa036d5f40
Camila Sáez <cfsaezbravo@gmail.com>
2017-05-08 14:05:59 -0300

    Initial commit

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var peso = document.getElementById("peso").value;
-var estatura = document.getElementById("estatura").value;
+
 
 function calcularpeso(){
+	var peso = document.getElementById("peso").value;
+	var estatura = document.getElementById("estatura").value;
 	var calculo = parseInt(peso*(estatura*2));
 	return calculo;
 }
